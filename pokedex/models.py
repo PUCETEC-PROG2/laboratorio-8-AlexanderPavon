d37672a3c37ccbaa2458d5894893fab8f81d0be5
pucetec <a.alexanderpavon@gmail.com>
2024-07-06 10:10:50 -0500

    Display,views,index,urls

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. MODELS SON CLASES QUE AYUDAN A GESTIONAR LA CONEXION CON LA BASE DE DATOS
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30,null=False)
+    type = models.CharField(max_length=30,null=False)
+    weight = models.IntegerField(null=False)
+    height = models.IntegerField(null=False)
+    
+    def __str__(self) -> str:
+        return self.name
